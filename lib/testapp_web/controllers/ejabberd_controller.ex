ec9e06b5bf55f6631a06737d710bb03b68f0d445
siddhant3030 <ssiddhant3030@gmail.com>
2020-05-11 10:06:32 +0530

    some changes

--- lib/testapp_web/controllers/ejabberd_controller.ex
+++ lib/testapp_web/controllers/ejabberd_controller.ex
@@ -1,3 +1,15 @@
-defmodule TestappWeb.EjabberdController do
-  use TestappWeb, :controller
-end
+# defmodule TestappWeb.EjabberdController do
+#   use TestappWeb, :controller
+
+#   require Record
+#   Record.defrecord :jid, Record.extract(:jid, from: "deps/xmpp/include/jid.hrl")
+
+#   plug :action
+
+#   def index(conn, _params) do
+#     # get online jid, parse and extract the user part.
+#     online_users = :ejabberd_sm.connected_users
+#                       |> Enum.map &(jid(:jid.decode(&1), :user))
+#     render conn, "index.html", users: online_users
+#   end
+# end
